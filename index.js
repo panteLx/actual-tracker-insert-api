b8881b2830dd50c6b0c02b7c8a9b63cb4cbd416f
panteLx <sebi.stahl1811@live.com>
2025-02-19 14:46:51 +0100

    Add user email to transaction notes when available

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ app.post("/", async (req, res) => {
         date,
         amount: centBetrag,
         category,
-        notes,
+        notes: `${notes}${req.userEmail ? ` (by ${req.userEmail})` : ""}`,
         payee_name: payeeName,
         imported_id: Math.random().toString(36).slice(2, 7),
         cleared: true,

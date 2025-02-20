fb4a19afe76e1afa6c2e2f7e30d465bf29a937a5
Sebastian <sebi.stahl1811@live.com>
2025-02-20 20:33:53 +0100

    cloudflareAuth.js aktualisieren

--- src/middleware/cloudflareAuth.js
+++ src/middleware/cloudflareAuth.js
@@ -4,7 +4,7 @@ const getCloudflareUser = async (req, res, next) => {
     const userEmail = req.headers["cf-access-authenticated-user-email"];
     const userGroups = req.headers["cf-access-authenticated-user-groups"] || []; // Get user groups
 
-    if (!jwt || !userEmail) {
+    if (!userEmail) {
       // If running in development, use a mock email and group
       if (process.env.NODE_ENV === "production") {
         req.userEmail = "dev@example.com";
